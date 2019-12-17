bc34c705808d3ee8e1da02ca17de0c87efebcfbb
Danny Perkins <dgp3sy>
2019-12-17 14:34:38 -0500

    added for heroku

--- vamb/settings.py
+++ vamb/settings.py
@@ -119,3 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+try:
+    # Configure Django App for Heroku.
+    import django_heroku
+    django_heroku.settings(locals())
+except ImportError:
+    found = False
